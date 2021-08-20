ad503be97faa4e62cadbdf96aa9de6433f8e29a4
MASHOD0 <mashhood2002@gmail.com>
2021-08-20 17:30:45 +0530

    added ,  and

--- src/DB/query.py
+++ src/DB/query.py
@@ -3,3 +3,16 @@ get_section_id = """
                 FROM sections
                 WHERE sections = '{}';
                 """
+get_student_pw="""select distinct password
+                from student
+                where "USN" = '{}' 
+                limit  1;
+                """
+add_new_student = """
+                INSERT INTO student(student_id, section_id, "USN", "Name", password, email, branch)
+                VALUES (DEFAULT, {}, '{}', '{}', '{}', '{}', '{}');
+                """
+add_new_teacher = """
+                INSERT INTO teachers (teacher_id, name, password, email, department) 
+                VALUES (DEFAULT, '{}', '{}', '{}', '{}');
+                 """
