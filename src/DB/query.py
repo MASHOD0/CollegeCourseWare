8371958eb93f9c7a4d3ba60b2d92d318b957f4f3
MASHOD0 <mashhood2002@gmail.com>
2021-08-21 18:03:59 +0530

    get_class_stud

--- src/DB/query.py
+++ src/DB/query.py
@@ -14,7 +14,7 @@ get_teacher_pw="""
                 where name = '{}' 
                 limit  1;
                 """
-                
+
 add_new_student = """
                 INSERT INTO student(student_id, section_id, "USN", "Name", password, email, branch)
                 VALUES (DEFAULT, {}, '{}', '{}', '{}', '{}', '{}');
@@ -23,3 +23,11 @@ add_new_teacher = """
                 INSERT INTO teachers (teacher_id, name, password, email, department) 
                 VALUES (DEFAULT, '{}', '{}', '{}', '{}');
                  """
+get_class_stud = """
+                select class_id,classes.course_id, "link", "time", courses.course_code, department
+                from classes
+                join courses
+                ON classes.course_id = courses.course_id
+                where section_id = (select section_id from student where "USN" = '{}' limit 1)
+                AND day = '{}';
+                """
